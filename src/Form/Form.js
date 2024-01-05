ad5f6ffc58024e29dc5662689070532e3b7e6225
Saurav <sauravkumar00108@gmail.com>
2024-01-05 22:04:23 +0530

    changes

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,129 +1,137 @@
 import React, { useState, useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import './Form.css';
+import "./Form.css";
 import Swal from "sweetalert2";
 import FormInput from "../Formitem/FormInput.js";
 import FormDataRow from "../FormData/FormDataRow.js";
 
 const Form = () => {
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        fatherName: "",
-        motherName: "",
-        address: "",
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    fatherName: "",
+    motherName: "",
+    address: "",
+  });
+  const [showModal, setShowModal] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [submittedData, setSubmittedData] = useState([]);
+  const [editedIndex, setEditedIndex] = useState(null);
+  const [recordsToShow, setRecordsToShow] = useState(3);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("submittedData");
+    if (savedData) {
+      setSubmittedData(JSON.parse(savedData));
+    }
+  }, []);
+
+  const openModalToAdd = () => {
+    setIsEditing(false);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      fatherName: "",
+      motherName: "",
+      address: "",
+    });
+    setShowModal(true);
+  };
+
+  const handleChange = (e, fieldName, index) => {
+    const { value } = e.target;
+    if (index !== undefined) {
+      const updatedData = submittedData.map((data, dataIndex) =>
+        dataIndex === index ? { ...data, [fieldName]: value } : data
+      );
+      setSubmittedData(updatedData);
+    } else {
+      setFormData({
+        ...formData,
+        [fieldName]: value,
       });
-      const [showModal, setShowModal] = useState(false);
-      const [isEditing, setIsEditing] = useState(false);
-      const [submittedData, setSubmittedData] = useState([]);
-      const [editedIndex, setEditedIndex] = useState(null);
-      const [recordsToShow, setRecordsToShow] = useState(3);
-    
-      useEffect(() => {
-        const savedData = localStorage.getItem("submittedData");
-        if (savedData) {
-          setSubmittedData(JSON.parse(savedData));
-        }
-      }, []);
-    
-      const openModalToAdd = () => {
-        setIsEditing(false);
-        setFormData({
-          firstName: "",
-          lastName: "",
-          fatherName: "",
-          motherName: "",
-          address: "",
-        });
-        setShowModal(true);
-      };
-    
-      const handleChange = (e, fieldName, index) => {
-        const { value } = e.target;
-        if (index !== undefined) {
-          const updatedData = submittedData.map((data, dataIndex) =>
-            dataIndex === index ? { ...data, [fieldName]: value } : data
-          );
-          setSubmittedData(updatedData);
-        } else {
-          setFormData({
-            ...formData,
-            [fieldName]: value,
-          });
-        }
-      };
-    
-      const handleEdit = (index) => {
-        setEditedIndex(index);
-        setIsEditing(true);
-        setFormData(submittedData[index]);
-        // setShowModal(true);
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        if (isEditing) {
-          const updatedSubmittedData = submittedData.map((data, index) =>
-            index === editedIndex ? formData : data
-          );
-          setSubmittedData(updatedSubmittedData);
-          toast.success("Data updated successfully!", { position: "top-center" });
-        } else {
-          const updatedSubmittedData = [...submittedData, formData];
-          setSubmittedData(updatedSubmittedData);
-          toast.success("Data added successfully!", { position: "top-center" });
-        }
-        setShowModal(false);
-        setEditedIndex(null);
-        setIsEditing(false);
-        setFormData({
-          firstName: "",
-          lastName: "",
-          fatherName: "",
-          motherName: "",
-          address: "",
-        });
-        localStorage.setItem("submittedData", JSON.stringify(submittedData));
-      };
-    
-      const handleDelete = (index) => {
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            const updatedSubmittedData = submittedData.filter((_, i) => i !== index);
-            setSubmittedData(updatedSubmittedData);
-            localStorage.setItem("submittedData", JSON.stringify(updatedSubmittedData));
-    
-            Swal.fire({
-              title: "Deleted!",
-              text: "Your file has been deleted.",
-              icon: "success"
-            });
-          }
-        });
-      };
-      const handleSave = (index) => {
-        const updatedData = submittedData.map((data, dataIndex) =>
-          dataIndex === index ? formData : data
+    }
+  };
+
+  const handleEdit = (index) => {
+    setEditedIndex(index);
+    setIsEditing(true);
+    setFormData(submittedData[index]);
+    // setShowModal(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isEditing) {
+      const updatedSubmittedData = submittedData.map((data, index) =>
+        index === editedIndex ? formData : data
+      );
+      setSubmittedData(updatedSubmittedData);
+      toast.success("Data updated successfully!", { position: "top-center" });
+    } else {
+      const updatedSubmittedData = [...submittedData, formData];
+      setSubmittedData(updatedSubmittedData);
+      toast.success("Data added successfully!", { position: "top-center" });
+    }
+    setShowModal(false);
+    setEditedIndex(null);
+    setIsEditing(false);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      fatherName: "",
+      motherName: "",
+      address: "",
+    });
+    localStorage.setItem("submittedData", JSON.stringify(submittedData));
+  };
+
+  const handleDelete = (index) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const updatedSubmittedData = submittedData.filter(
+          (_, i) => i !== index
         );
-        setSubmittedData(updatedData);
-        localStorage.setItem("submittedData", JSON.stringify(updatedData));
-        setEditedIndex(null);
-        toast.success("Data updated successfully!", { position: "top-center" });
-      };
-      const handleRecordsToShowChange = (e) => {
-        const newRecordsToShow = parseInt(e.target.value, 10);
-        setRecordsToShow(newRecordsToShow);
-      };
-    
+        setSubmittedData(updatedSubmittedData);
+        localStorage.setItem(
+          "submittedData",
+          JSON.stringify(updatedSubmittedData)
+        );
+
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+  const handleSave = (index) => {
+    const updatedData = submittedData.map((data, dataIndex) =>
+      dataIndex === index ? formData : data
+    );
+    setSubmittedData(updatedData);
+    localStorage.setItem("submittedData", JSON.stringify(updatedData));
+    setEditedIndex(null);
+    toast.success("Data updated successfully!", { position: "top-center" });
+  };
+  const handleRecordsToShowChange = (e) => {
+    const newRecordsToShow = parseInt(e.target.value, 10);
+    setRecordsToShow(newRecordsToShow);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
@@ -177,8 +185,11 @@ const Form = () => {
                 onChange={handleChange}
                 required
               />
-              
+
               <button type="submit">Submit</button>
+              <button type="cancel" onClick={handleCancel}>
+                Cancel
+              </button>
             </form>
           </div>
         </div>
@@ -212,7 +223,9 @@ const Form = () => {
             ))}
           </tbody>
           <div className="Pagination">
-            <h3 className="h3">Visible Record {recordsToShow} - {submittedData.length}</h3>
+            <h3 className="h3">
+              Visible Record {recordsToShow} - {submittedData.length}
+            </h3>
             <input
               type="number"
               min="1"
